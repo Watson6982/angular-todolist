d8c7acad3075ea1688c775c5edc7f99c69a342a0
Watson6982 <watson6982@hotmail.co.uk>
2020-08-02 21:51:35 +0100

    Add custom method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,6 +9,10 @@ export class AppComponent {
   title: string = 'Todo list';
   name: string = 'Josh';
   constructor() {
-    console.log('Running on init');
+    console.log('Running on init ' + this.name);
+    this.changeName('Joshua');
+  }
+  changeName(name: string): void {
+    this.name = name;
   }
 }
