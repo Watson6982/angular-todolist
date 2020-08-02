24f279af77e7a17f093a2f45fad2f5c353bcbc8a
Watson6982 <watson6982@hotmail.co.uk>
2020-08-02 22:55:39 +0100

    add http client to retrive todos from rest service

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,30 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Todo } from '../models/Todo';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
+  todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
+  todosLimit: string = '?_limit=5';
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
-
-  getTodos() {
-    return [
-      {
-        id: 1,
-        title: 'Todo one',
-        completed: false
-      },
-      {
-        id: 2,
-        title: 'Todo two',
-        completed: true
-      },
-      {
-        id: 3,
-        title: 'Todo three',
-        completed: false
-      }
-    ]
+  getTodos(): Observable<Todo[]> {
+    return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
   }
 
 }
