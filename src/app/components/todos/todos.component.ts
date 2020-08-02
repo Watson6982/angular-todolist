4a091b9c71b764907e21d35c9744b1e3b3223bfe
Watson6982 <watson6982@hotmail.co.uk>
2020-08-02 23:49:23 +0100

    add functionality to add todo

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -27,4 +27,12 @@ export class TodosComponent implements OnInit {
     this.todoService.deleteTodo(todo).subscribe();
   }
 
+  addTodo(todo: Todo) {
+    // add to server
+    this.todoService.addTodo(todo).subscribe(todo => {
+      // add to ui on response
+      this.todos.push(todo);
+    })
+  }
+
 }
