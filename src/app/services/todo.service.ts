8c464af33d342453d2178f3a996fa5cf4f7b6e0c
Watson6982 <watson6982@hotmail.co.uk>
2020-08-02 23:11:40 +0100

    add put request for toggle complete

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 import { Todo } from '../models/Todo';
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +16,18 @@ import { Todo } from '../models/Todo';
 export class TodoService {
   todosUrl: string = 'https://jsonplaceholder.typicode.com/todos';
   todosLimit: string = '?_limit=5';
+
   constructor(private http: HttpClient) { }
 
+  // Get Todos
   getTodos(): Observable<Todo[]> {
     return this.http.get<Todo[]>(`${this.todosUrl}${this.todosLimit}`);
+  }
+
+  // Toggle Completed
+  toggleComplete(todo: Todo): Observable<any> {
+    const url = `${this.todosUrl}/${todo.id}`;
+    return this.http.put(url, todo, httpOptions);
   }
 
 }
